f1360e963f54bc86f2b4046a03be22d2080ce922
filipe oliveira <filipecosta.90@gmail.com>
2021-02-10 13:18:42 +0000

    Added parseExporterTimeMetricDefinition() (#38)
    * [add] Added parseExporterTimeMetricDefinition()
    
    * Bumping version from 0.1.37 to 0.1.38

--- redisbench_admin/utils/benchmark_config.py
+++ redisbench_admin/utils/benchmark_config.py
@@ -1,6 +1,15 @@
 from jsonpath_ng import parse
 
 
+<<<<<<< HEAD
+def parseExporterMetricsDefinition(
+    benchmark_config: dict, configkey: str = "redistimeseries"
+):
+    metrics = []
+    if configkey in benchmark_config:
+        if "metrics" in benchmark_config[configkey]:
+            for metric_name in benchmark_config[configkey]["metrics"]:
+=======
 def parseExporterMetricsDefinition(benchmark_config, configkey="redistimeseries"):
     metrics = []
     if configkey in benchmark_config:
@@ -8,10 +17,25 @@ def parseExporterMetricsDefinition(benchmark_config, configkey="redistimeseries"
             for metric_name in benchmark_config[configkey][
                 "metrics"
             ]:
+>>>>>>> origin/master
                 metrics.append(metric_name)
     return metrics
 
 
+<<<<<<< HEAD
+def parseExporterTimeMetricDefinition(
+    benchmark_config: dict, configkey: str = "redistimeseries"
+):
+    metricPath = None
+    if "timemetric" in benchmark_config[configkey]:
+        metricPath = benchmark_config[configkey]["timemetric"]
+    return metricPath
+
+
+def parseExporterTimeMetric(metricPath: str, results_dict: dict):
+    jsonpath_expr = parse(metricPath)
+    datapoints_timestamp = int(jsonpath_expr.find(results_dict)[0].value)
+=======
 def parseExporterTimeMetric(benchmark_config, results_dict, configkey="redistimeseries"):
     datapoints_timestamp = None
     if "timemetric" in benchmark_config[configkey]:
@@ -20,4 +44,5 @@ def parseExporterTimeMetric(benchmark_config, results_dict, configkey="redistime
         datapoints_timestamp = int(
             jsonpath_expr.find(results_dict)[0].value
         )
+>>>>>>> origin/master
     return datapoints_timestamp
