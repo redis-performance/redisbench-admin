846dc05e22a0928fe9475ca1058590c5b68fb0bb
filipecosta90 <filipecosta.90@gmail.com>
2021-02-10 13:16:46 +0000

    [add] Added parseExporterTimeMetricDefinition()

--- redisbench_admin/utils/benchmark_config.py
+++ redisbench_admin/utils/benchmark_config.py
@@ -1,23 +1,27 @@
 from jsonpath_ng import parse
 
 
-def parseExporterMetricsDefinition(benchmark_config, configkey="redistimeseries"):
+def parseExporterMetricsDefinition(
+    benchmark_config: dict, configkey: str = "redistimeseries"
+):
     metrics = []
     if configkey in benchmark_config:
         if "metrics" in benchmark_config[configkey]:
-            for metric_name in benchmark_config[configkey][
-                "metrics"
-            ]:
+            for metric_name in benchmark_config[configkey]["metrics"]:
                 metrics.append(metric_name)
     return metrics
 
 
-def parseExporterTimeMetric(benchmark_config, results_dict, configkey="redistimeseries"):
-    datapoints_timestamp = None
+def parseExporterTimeMetricDefinition(
+    benchmark_config: dict, configkey: str = "redistimeseries"
+):
+    metricPath = None
     if "timemetric" in benchmark_config[configkey]:
-        tspath = benchmark_config[configkey]["timemetric"]
-        jsonpath_expr = parse(tspath)
-        datapoints_timestamp = int(
-            jsonpath_expr.find(results_dict)[0].value
-        )
+        metricPath = benchmark_config[configkey]["timemetric"]
+    return metricPath
+
+
+def parseExporterTimeMetric(metricPath: str, results_dict: dict):
+    jsonpath_expr = parse(metricPath)
+    datapoints_timestamp = int(jsonpath_expr.find(results_dict)[0].value)
     return datapoints_timestamp
