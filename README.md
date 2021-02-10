4ecfc05f37bd7f13901b4f482347216a16a89c08
filipe oliveira <filipecosta.90@gmail.com>
2021-02-10 14:14:54 +0000

    Updated project description and fixed benchmark config utilities (#39)

--- README.md
+++ README.md
@@ -5,19 +5,20 @@
 # redisbench-admin
 
 Redis benchmark run helper can help you with the following tasks:
-
+- Setup abd teardown of benchmarking infrastructure specified on [RedisLabsModules/testing-infrastructure](https://github.com/RedisLabsModules/testing-infrastructure)
 - Setup and teardown of an Redis and Redis Modules DBs for benchmarking
 - Management of benchmark data and specifications across different setups
 - Running benchmarks and recording results
-- Comparing performance results
 - Exporting performance results in several formats (CSV, RedisTimeSeries, JSON)
+- [SOON] Comparing performance results
 - [SOON] Finding performance problems by attaching telemetry probes
 
 Current supported benchmark tools:
-- [redisgraph-database-benchmark](https://github.com/RedisGraph/graph-database-benchmark/tree/master/benchmark/redisgraph)
+- [redisgraph-benchmark-go](https://github.com/RedisGraph/redisgraph-benchmark-go)
 - [ftsb_redisearch](https://github.com/RediSearch/ftsb)
-
-** future versions will also support redis-benchmark and memtier_benchmark.
+- [redis-benchmark](https://github.com/redis/redis)
+- [SOON][memtier_benchmark](https://github.com/RedisLabs/memtier_benchmark)
+- [SOON][aibench](https://github.com/RedisAI/aibench)
 
 ## Installation
 
--- redisbench_admin/utils/benchmark_config.py
+++ redisbench_admin/utils/benchmark_config.py
@@ -1,7 +1,6 @@
 from jsonpath_ng import parse
 
 
-<<<<<<< HEAD
 def parseExporterMetricsDefinition(
     benchmark_config: dict, configkey: str = "redistimeseries"
 ):
@@ -9,20 +8,10 @@ def parseExporterMetricsDefinition(
     if configkey in benchmark_config:
         if "metrics" in benchmark_config[configkey]:
             for metric_name in benchmark_config[configkey]["metrics"]:
-=======
-def parseExporterMetricsDefinition(benchmark_config, configkey="redistimeseries"):
-    metrics = []
-    if configkey in benchmark_config:
-        if "metrics" in benchmark_config[configkey]:
-            for metric_name in benchmark_config[configkey][
-                "metrics"
-            ]:
->>>>>>> origin/master
                 metrics.append(metric_name)
     return metrics
 
 
-<<<<<<< HEAD
 def parseExporterTimeMetricDefinition(
     benchmark_config: dict, configkey: str = "redistimeseries"
 ):
@@ -35,14 +24,4 @@ def parseExporterTimeMetricDefinition(
 def parseExporterTimeMetric(metricPath: str, results_dict: dict):
     jsonpath_expr = parse(metricPath)
     datapoints_timestamp = int(jsonpath_expr.find(results_dict)[0].value)
-=======
-def parseExporterTimeMetric(benchmark_config, results_dict, configkey="redistimeseries"):
-    datapoints_timestamp = None
-    if "timemetric" in benchmark_config[configkey]:
-        tspath = benchmark_config[configkey]["timemetric"]
-        jsonpath_expr = parse(tspath)
-        datapoints_timestamp = int(
-            jsonpath_expr.find(results_dict)[0].value
-        )
->>>>>>> origin/master
     return datapoints_timestamp
