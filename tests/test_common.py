ae0b4ed51a707f74fc23e9ffce4eed6690c56063
filipecosta90 <filipecosta.90@gmail.com>
2021-04-15 14:06:23 +0100

    [wip] Increased coverage of tool. General code improvement

--- tests/test_common.py
+++ tests/test_common.py
@@ -8,7 +8,8 @@ from redisbench_admin.export.common.common import (
     split_tags_string,
     get_or_None,
 )
-from redisbench_admin.run.common import get_start_time_vars, prepare_benchmark_parameters
+from redisbench_admin.run.common import get_start_time_vars, prepare_benchmark_parameters, \
+    extract_benchmark_tool_settings, common_exporter_logic, process_default_yaml_properties_file
 
 
 class Test(TestCase):
@@ -54,8 +55,34 @@ def test_get_start_time_vars():
 def test_prepare_benchmark_parameters():
     with open("./tests/test_data/ycsb-config.yml", "r") as yml_file:
         benchmark_config = yaml.safe_load(yml_file)
-        command_arr, command_str = prepare_benchmark_parameters(benchmark_config, "ycsb",  "6380", "localhost",
+        command_arr, command_str = prepare_benchmark_parameters(benchmark_config, "ycsb", "6380", "localhost",
                                                                 "out.txt", False)
         assert command_str == "ycsb load redisearch -P workloads/workload-ecommerce -p \"threadcount=64\"" \
                               " -p \"redis.host=localhost\" -p \"redis.port=6380\"" \
                               " -p \"recordcount=100000\" -p \"operationcount=100000\""
+
+
+def test_extract_benchmark_tool_settings():
+    config_files = ["./tests/test_data/ycsb-config.yml", "./tests/test_data/redis-benchmark.yml",
+                    "./tests/test_data/redisgraph-benchmark-go.yml"]
+    for file in config_files:
+        with open(file, "r") as yml_file:
+            benchmark_config = yaml.safe_load(yml_file)
+            benchmark_min_tool_version, benchmark_min_tool_version_major, benchmark_min_tool_version_minor, benchmark_min_tool_version_patch, benchmark_tool, benchmark_tool_source = extract_benchmark_tool_settings(
+                benchmark_config)
+            assert benchmark_tool is not None
+            prepare_benchmark_parameters(benchmark_config, benchmark_tool, "9999", "localhost", "out.txt", False)
+
+
+def test_common_exporter_logic():
+    # negative test
+    common_exporter_logic(None, None, None, None, None, None,
+                          None, None, None, None)
+
+
+def test_process_default_yaml_properties_file():
+    with open("./tests/test_data/common-properties-v0.1.yml", "r") as yml_file:
+        default_kpis, default_metrics, exporter_timemetric_path = process_default_yaml_properties_file({},{},"1.yml",None,yml_file)
+        assert exporter_timemetric_path == "$.StartTime"
+        assert default_kpis is None
+
---
+++ tests/test_redis_benchmark.py
@@ -0,0 +1,30 @@
+import shutil
+
+import yaml
+
+from redisbench_admin.run.redis_benchmark.redis_benchmark import prepareRedisBenchmarkCommand, \
+    redis_benchmark_ensure_min_version_local, redis_benchmark_from_stdout_csv_to_json
+
+
+def test_prepare_redis_benchmark_command():
+    with open("./tests/test_data/redisgraph-benchmark-go.yml", "r") as yml_file:
+        benchmark_config = yaml.safe_load(yml_file)
+        for k in benchmark_config["clientconfig"]:
+            if 'parameters' in k:
+                command_arr, command_str = prepareRedisBenchmarkCommand("redis-benchmark", "localhost",
+                                                                        "6380", k)
+                assert command_str == "redis-benchmark -h localhost -p 6380 --csv -e -c 32 --threads 4 -n 1000000"
+
+
+def test_redis_benchmark_ensure_min_version_local():
+    redis_benchmark_bin = shutil.which("redis-benchmark")
+    if redis_benchmark_bin:
+        redis_benchmark_ensure_min_version_local(redis_benchmark_bin, "6.2.0", "6", "2", "0")
+
+
+def test_redis_benchmark_from_stdout_csv_to_json():
+    with open("./tests/test_data/redis-benchmark-6.2.0-csv.out", "r") as csv_file:
+        csv_data = csv_file.read()
+        results_dict = redis_benchmark_from_stdout_csv_to_json(csv_data, 1, "1")
+        assert "SET" in results_dict["Tests"]
+        assert "GET" in results_dict["Tests"]
