98793a9697995c52f4a46d5aa9106e44cdae4d4e
filipe oliveira <filipecosta.90@gmail.com>
2021-04-22 10:56:07 +0100

    [fix] Fixed gh issue 83: redis-benchmark csv exporter parser conversion issues (#84)

--- tests/test_redis_benchmark_csv_format.py
+++ tests/test_redis_benchmark_csv_format.py
@@ -31,13 +31,18 @@ def test_redis_benchmark_export_logic():
         results_dict = redis_benchmark_from_stdout_csv_to_json(csv_data, 1, "1")
         redis_benchmark_export_logic(results_dict, [], None, {})
         assert "SET" in results_dict["Tests"]
+        assert "139082.06" == results_dict["Tests"]["SET"]["rps"]
         assert "GET" in results_dict["Tests"]
+        assert "136986.30" == results_dict["Tests"]["GET"]["rps"]
+        assert len("136986.30") == len(results_dict["Tests"]["GET"]["rps"])
 
     with open("./tests/test_data/redis-benchmark-6.2.0-csv.out.2", "r") as csv_file:
         csv_data = csv_file.read()
         results_dict = redis_benchmark_from_stdout_csv_to_json(csv_data, 1, "1")
         redis_benchmark_export_logic(results_dict, [], None, {})
         assert "JSON.SET" in results_dict["Tests"]
+        assert "73391.80" == results_dict["Tests"]["JSON.SET"]["rps"]
+
 
     with open("./tests/test_data/redis-benchmark-6.2.0-csv.out.2", "r") as csv_file:
         csv_data = csv_file.read()
