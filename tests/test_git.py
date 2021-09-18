ea7a3d55a52bc8aba0bed2a8f85bbb9919ec2fbf
filipe oliveira <filipecosta.90@gmail.com>
2021-09-18 01:09:02 +0100

    [fix] Fixed run_remote_client_tool() missing 4 required positional arguments: (#208)

---
+++ tests/test_git.py
@@ -0,0 +1,21 @@
+#  BSD 3-Clause License
+#
+#  Copyright (c) 2021., Redis Labs Modules
+#  All rights reserved.
+#
+from redisbench_admin.run.git import git_vars_crosscheck
+
+
+def test_git_vars_crosscheck():
+    (
+        tf_github_actor,
+        tf_github_branch,
+        tf_github_org,
+        tf_github_repo,
+        tf_github_sha,
+    ) = git_vars_crosscheck(None, None, None, None, None)
+    assert tf_github_actor != None
+    assert tf_github_branch != None
+    assert tf_github_org != None
+    assert tf_github_repo != None
+    assert tf_github_sha != None
