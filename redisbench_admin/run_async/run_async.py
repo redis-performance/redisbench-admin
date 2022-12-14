0b8ebb43c7d27a66f34f39b1f4c36b1ea7e3b25a
Anton Tokarev <anton.tokarev@softeq.com>
2022-12-14 19:14:17 +0200

    feature/async-run: command line, terraform install

--- redisbench_admin/run_async/run_async.py
+++ redisbench_admin/run_async/run_async.py
@@ -239,6 +239,19 @@ def run_async_command_logic(argv, args, project_name, project_version):
         [
             "mkdir -p work_dir",
             "tar xf {} -C work_dir".format(archive_name),
+            "apt update",
+            "apt install -y gnupg software-properties-common",
+            "wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee "
+            "/usr/share/keyrings/hashicorp-archive-keyring.gpg",
+
+            "gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint",
+
+            "echo \"deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] "
+            "https://apt.releases.hashicorp.com $(lsb_release -cs) main\" | sudo tee "
+            "/etc/apt/sources.list.d/hashicorp.list",
+
+            "apt update",
+            "apt install terraform",
             "curl -sSL https://install.python-poetry.org | POETRY_VERSION=1.2.2 python3 -",
             "cd work_dir/redisbench-admin && PATH=\"/home/ubuntu/.local/bin:$PATH\" poetry config "
             "virtualenvs.in-project true",
