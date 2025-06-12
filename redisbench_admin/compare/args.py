45a8bbd8003c8be8b4508a2554868706f9f82a8f
fcostaoliveira <filipe@redis.com>
2025-06-12 12:20:12 +0100

    compare improvements

--- redisbench_admin/compare/args.py
+++ redisbench_admin/compare/args.py
@@ -134,7 +134,7 @@ def create_compare_arguments(parser):
     parser.add_argument(
         "--regressions-percent-lower-limit",
         type=float,
-        default=5.0,
+        default=8.0,
         help="Only consider regressions with a percentage over the defined limit. (0-100)",
     )
     parser.add_argument(
