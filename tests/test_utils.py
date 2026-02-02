977a0293ccd3ee5ad0ae6ae173b5befd867dd326
Joan Fontanals Martinez <jfontanalsmartinez@gmail.com>
2026-02-02 13:42:13 +0000

    do not always use tmp/input.data

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,6 +3,7 @@ from unittest import TestCase
 from redisbench_admin.utils.utils import (
     retrieve_local_or_remote_input_json,
     get_ts_metric_name,
+    get_remote_input_file_from_url,
 )
 
 
@@ -115,3 +116,40 @@ def test_get_ts_metric_name():
     ) == "ci.benchmarks.redislabs/by.branch/ci/redis/redis/test-1/{}/oss-standalone/unstable/rps/PING".format(
         build_variant_name
     )
+
+
+def test_get_remote_input_file_from_url():
+    """Test that get_remote_input_file_from_url generates unique file paths based on file name."""
+    # Test with a typical S3 URL
+    url = "https://s3.amazonaws.com/benchmarks.redislabs/redistimeseries/tsbs/queries/SETUP.CSV"
+    assert get_remote_input_file_from_url(url) == "/tmp/input-SETUP.data"
+
+    # Test with a different file name (simulates the AND_QUERY case)
+    url = "https://s3.amazonaws.com/benchmarks.redislabs/redisearch/data/AND_QUERY.CSV"
+    assert get_remote_input_file_from_url(url) == "/tmp/input-AND_QUERY.data"
+
+    # Test with URL-encoded characters
+    url = "https://s3.amazonaws.com/benchmarks.redislabs/tsbs/queries/queries_cpu-only_redistimeseries_100_cpu-max-all-1_10000_123_2016-01-01T00%3A00%3A00Z_2016-01-04T00%3A00%3A00Z.dat"
+    result = get_remote_input_file_from_url(url)
+    assert result.startswith("/tmp/input-")
+    assert result.endswith(".data")
+    # URL-encoded characters like %3A should be replaced with _
+    assert "%" not in result
+
+    # Test with s3:// URI
+    url = "s3://benchmarks.redislabs/data/my_dataset.csv"
+    assert get_remote_input_file_from_url(url) == "/tmp/input-my_dataset.data"
+
+    # Test with None - should fallback to default
+    assert get_remote_input_file_from_url(None) == "/tmp/input.data"
+
+    # Test with empty string - should fallback to default
+    assert get_remote_input_file_from_url("") == "/tmp/input.data"
+
+    # Test with file without extension
+    url = "https://s3.amazonaws.com/data/myfile"
+    assert get_remote_input_file_from_url(url) == "/tmp/input-myfile.data"
+
+    # Test with .dat extension
+    url = "https://s3.amazonaws.com/data/benchmark_data.dat"
+    assert get_remote_input_file_from_url(url) == "/tmp/input-benchmark_data.data"
