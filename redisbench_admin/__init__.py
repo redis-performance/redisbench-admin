ee120f0ac9346adae08d3e57beaf295cf0175732
Joan Fontanals <jfontanalsmartinez@gmail.com>
2026-02-09 10:32:30 +0100

    deprecate pkg-resources (#497)
    * add keyspacelength_min option
    
    * deprecate pkg-resources
    
    * fix black
    
    * add setutools to poetry
    
    * update pyproject

--- redisbench_admin/__init__.py
+++ redisbench_admin/__init__.py
@@ -6,9 +6,16 @@
 
 # This attribute is the only one place that the version number is written down,
 # so there is only one place to change it when the version number changes.
-import pkg_resources
 
 try:
-    __version__ = pkg_resources.get_distribution("redisbench-admin").version
-except (pkg_resources.DistributionNotFound, AttributeError):
+    from importlib.metadata import version
+except ImportError:
+    try:  # For Python<3.8
+        from importlib_metadata import version  # type: ignore
+    except ImportError:
+        version = None
+
+try:
+    __version__ = version("redisbench-admin")
+except Exception:
     __version__ = "99.99.99"  # like redis modules
