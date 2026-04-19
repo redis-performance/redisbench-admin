7e98b3b95ade070c81fd8a6644a9f9416c4399d5
Joan Fontanals <jfontanalsmartinez@gmail.com>
2026-04-20 00:48:33 +0200

    increase infra wait time (#504)
    * increase infra wait time
    
    * bump version

--- redisbench_admin/utils/remote.py
+++ redisbench_admin/utils/remote.py
@@ -431,7 +431,7 @@ def setup_remote_environment(
         # Re-raise the exception with more context
         raise
 
-    infra_wait_secs = 30
+    infra_wait_secs = 60
     logging.warning(f"Infra ready wait... for {infra_wait_secs} secs")
     time.sleep(infra_wait_secs)
     return retrieve_tf_connection_vars(return_code, tf)
